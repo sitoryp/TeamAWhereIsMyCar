ee5493ca73c20d4c0c1b7f53117b6713e8acc1b9
sitoryp <d.lewis1987@yahoo.com>
2016-11-09 22:42:21 -0500

    Setup some functionality to get the current location from the Set Location button added by David
    Still more to go, I added a textview with the id "@+id/testing"
    temporarily so that I could verify the location being collected

--- app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
+++ app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
@@ -1,24 +1,94 @@
 package mlb402.teamawhereismycar;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-protected Button aboutButton;
+
+    protected Button aboutButton;
+    protected Button storeLocation;
+    protected LocationManager locationListener;
+    protected Location currentLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        aboutButton = (Button)findViewById(R.id.aboutButton);
-        aboutButton.setOnClickListener(new View.OnClickListener(){
+
+        setupUI();
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.about_layout);
 
             }
         });
+
+        storeLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationListener = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                currentLocation = locationListener.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                TextView testing = (TextView) findViewById(R.id.testing);
+                testing.setText(currentLocation.toString());
+            }
+        });
+
+
+
+//        locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderEnabled(String provider) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderDisabled(String provider) {
+//
+//            }
+//
+//        };
     }
 
+
+
+    private void setupUI(){
+        aboutButton = (Button)findViewById(R.id.aboutButton);
+        storeLocation = (Button)findViewById(R.id.setLocationButton);
+
+        //adding this to request permission from the user. . .
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    }
 }
