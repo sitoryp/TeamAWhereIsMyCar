d44b0e08cfeeb02a415a0ab88cc60cffa8155da3
viggy815 <viggy815@aol.com>
2016-11-09 21:41:24 -0500

    I started on the layout and made some updates

--- app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
+++ app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
@@ -2,12 +2,23 @@ package mlb402.teamawhereismycar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+protected Button aboutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutButton = (Button)findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.about_layout);
+
+            }
+        });
     }
+
 }
