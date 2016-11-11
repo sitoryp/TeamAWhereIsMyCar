68591ec8e9a04f22dac838bb4648e2c931ce65e1
sitoryp <d.lewis1987@yahoo.com>
2016-11-10 22:37:59 -0500

    removed some unused imports (stuff I added when testing and trying different things. . .)
    In an attempt to keep the file size down.

--- app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
+++ app/src/main/java/mlb402/teamawhereismycar/MainActivity.java
@@ -8,16 +8,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
 
